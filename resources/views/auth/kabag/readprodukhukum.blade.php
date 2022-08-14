3e53403cb5296dece2b6dc7cd02b4f16fe0401fe
Muhammad Ferian Rizky Akbari <muhammadferian@gmail.com>
2022-08-15 06:00:27 +0700

    edit download file interface

--- resources/views/auth/kabag/readprodukhukum.blade.php
+++ resources/views/auth/kabag/readprodukhukum.blade.php
@@ -1,11 +1,11 @@
 @extends('auth.main')
 
 @section('content')
-@if(session()->has('success'))  
-  <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
-  {{ session()->get('success') }}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>
+@if(session()->has('success'))
+<div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
+    {{ session()->get('success') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
 @endif
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Detail Produk Hukum</h1>
@@ -18,12 +18,12 @@
         <input type="text" class="form-control input mt-2" value="{{ $draft->kasubagUndang->staffUndang->admin->draft->user->name }}" readonly>
 
         <div class="fs-6 mt-3">
-            Jenis / Bentuk Peraturan 
+            Jenis / Bentuk Peraturan
         </div>
         <input type="text" class="form-control input mt-2" value="{{ $draft->kasubagUndang->staffUndang->admin->draft->jenis }}" readonly>
 
         <div class="fs-6 mt-3">
-            Judul Produk Hukum 
+            Judul Produk Hukum
         </div>
         <input type="text" class="form-control input mt-2" value="{{ $draft->kasubagUndang->staffUndang->admin->draft->judul }}" readonly>
 
@@ -46,23 +46,68 @@
                 <div class="fs-6 mt-3">
                     Surat Pengajuan
                 </div>
-                <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->surat_pengajuan)}}" class="btn btn-primary mt-2">Download</a>
+                <div class="row mt-2">
+                    <div class="col-11 col-sm-8 col-md-7 col-lg-8 col-xl-8 rounded-3 border-1 border border-dark px-2 py-2 ms-3" style="background-color: #e9ecef">
+                        <div class="row">
+                            <div class="col-1">
+                                <i class="fa-solid fa-file"></i>
+                            </div>
+                            <div class="col-10">
+                                <input type="text" value="{{ $draft->kasubagUndang->staffUndang->admin->draft->surat_pengajuan }}" style="border: none; background-color: #e9ecef; width: 105%;" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->surat_pengajuan )}}" class="ms-2" style="vertical-align: -webkit-baseline-middle">Download</a>
+                    </div>
+                </div>
+                <!-- <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->surat_pengajuan )}}" class="btn btn-primary mt-2">Download</a> -->
             </div>
         </div>
 
         <div class="row">
-            <div class="col-4">
+            <div class="col-5 col-sm-6 col-md-6 col-lg-6 col-xl-6">
                 <div class="fs-6 mt-3">
                     Draft Produk Hukum
                 </div>
-                <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum)}}" class="btn btn-primary mt-2">Download</a>
+                <div class="row mt-2">
+                    <div class="col-11 col-sm-8 col-md-7 col-lg-8 col-xl-8 rounded-3 border-1 border border-dark px-2 py-2 ms-3" style="background-color: #e9ecef">
+                        <div class="row">
+                            <div class="col-1">
+                                <i class="fa-solid fa-file"></i>
+                            </div>
+                            <div class="col-9 col-sm-10 col-md-10 col-lg-10 col-xl-10">
+                                <input type="text" value="{{ $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum }}" style="border: none; background-color: #e9ecef; width: 105%;" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum )}}" class="ms-2" style="vertical-align: -webkit-baseline-middle">Download</a>
+                    </div>
+                </div>
+                <!-- <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum)}}" class="btn btn-primary mt-2">Download</a> -->
             </div>
             @if($draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum_lama)
-            <div class="col-4">
+            <div class="col">
                 <div class="fs-6 mt-3">
                     Draft Produk Hukum Lama
                 </div>
-                <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum_lama)}}" class="btn btn-primary mt-2">Download</a>
+                <div class="row mt-2">
+                    <div class="col-11 col-sm-8 col-md-7 col-lg-8 col-xl-8 rounded-3 border-1 border border-dark px-2 py-2 ms-3" style="background-color: #e9ecef">
+                        <div class="row">
+                            <div class="col-1">
+                                <i class="fa-solid fa-file"></i>
+                            </div>
+                            <div class="col-10">
+                                <input type="text" value="{{ $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum_lama }}" style="border: none; background-color: #e9ecef; width: 105%;" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum_lama )}}" class="ms-2" style="vertical-align: -webkit-baseline-middle">Download</a>
+                    </div>
+                </div>
+                <!-- <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->admin->draft->draft_produk_hukum_lama)}}" class="btn btn-primary mt-2">Download</a> -->
             </div>
             @endif
         </div>
@@ -72,7 +117,22 @@
                 <div class="fs-6 mt-3">
                     Revisi Produk Hukum
                 </div>
-                <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->revisi_produk_hukum)}}" class="btn btn-primary mt-2">Download</a>
+                <div class="row mt-2">
+                    <div class="col-11 col-sm-8 col-md-7 col-lg-8 col-xl-8 rounded-3 border-1 border border-dark px-2 py-2 ms-3" style="background-color: #e9ecef">
+                        <div class="row">
+                            <div class="col-1">
+                                <i class="fa-solid fa-file"></i>
+                            </div>
+                            <div class="col-10">
+                                <input type="text" value="{{ $draft->kasubagUndang->staffUndang->revisi_produk_hukum }}" style="border: none; background-color: #e9ecef; width: 105%;" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->revisi_produk_hukum )}}" class="ms-2" style="vertical-align: -webkit-baseline-middle">Download</a>
+                    </div>
+                </div>
+                <!-- <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->revisi_produk_hukum)}}" class="btn btn-primary mt-2">Download</a> -->
             </div>
         </div>
 
@@ -81,18 +141,32 @@
                 <div class="fs-6 mt-3">
                     NPKND
                 </div>
-                <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->npknd)}}" class="btn btn-primary mt-2">Download</a>
+                <div class="row mt-2">
+                    <div class="col-11 col-sm-8 col-md-7 col-lg-8 col-xl-8 rounded-3 border-1 border border-dark px-2 py-2 ms-3" style="background-color: #e9ecef">
+                        <div class="row">
+                            <div class="col-1">
+                                <i class="fa-solid fa-file"></i>
+                            </div>
+                            <div class="col-10">
+                                <input type="text" value="{{ $draft->kasubagUndang->staffUndang->npknd }}" style="border: none; background-color: #e9ecef; width: 105%;" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->npknd )}}" class="ms-2" style="vertical-align: -webkit-baseline-middle">Download</a>
+                    </div>
+                </div>
+                <!-- <a href="{{ asset('storage/' . $draft->kasubagUndang->staffUndang->npknd)}}" class="btn btn-primary mt-2">Download</a> -->
             </div>
         </div>
-            
+
         @if($draft->status == 'menunggu' || $draft->status == 'ditolak oleh sekda' || $draft->status == 'ditolak oleh walikota')
         <form action="/dashboard/kabag/process/{{$draft->id}}" method="post">
             @csrf
             <div class="fs-6 mt-3">
                 Keterangan
-             </div>
-            <textarea type="text" class="form-control input mt-2" 
-            name="keterangan" id="keterangan" style="height:100px">{{ old('keterangan') }}</Textarea>
+            </div>
+            <textarea type="text" class="form-control input mt-2" name="keterangan" id="keterangan" style="height:100px">{{ old('keterangan') }}</Textarea>
 
             @if($draft->keterangan_penolakan)
             <div class="fs-6 mt-3">
@@ -115,7 +189,7 @@
             </div>
         </form>
         @endif
-        
+
         @if($draft->status == 'diterima')
         <div class="d-grid gap-2 mt-4 mb-4">
             <a href="/dashboard" class="btn btn-secondary">Kembali</a>
